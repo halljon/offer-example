ec7fb2885a549929d1d54b2266c1752aea38d48a
halljon <jon@blendedlogic.com>
2018-05-26 14:00:38 +0100

    #16 - fleshing out service and controller layers without implementations.

--- offer-example-service/src/main/java/io/halljon/offerexample/offer/service/OfferService.java
+++ offer-example-service/src/main/java/io/halljon/offerexample/offer/service/OfferService.java
@@ -1,18 +1,16 @@
-package io.halljon.offerexample.offer.repository;
+package io.halljon.offerexample.offer.service;
 
 import io.halljon.offerexample.offer.domain.Offer;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 
-public interface OfferRepository {
-    void saveOffer(Offer offer);
+public interface OfferService {
+    String saveOffer(Offer offer);
 
     void cancelOffer(String merchantIdentifier,
                      String offerIdentifier);
 
-    Collection<Offer> findActiveOffers(String merchantIdentifier,
-                                       LocalDateTime dateTime);
+    Collection<Offer> findActiveOffers(String merchantIdentifier);
 
     Collection<Offer> findAllOffers(String merchantIdentifier);
 
